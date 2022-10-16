584f6ed210da96d590049b8513f7df5ddb5a9fe6
Welbert Castro <welberthime@gmail.com>
2022-10-15 21:46:29 -0300

    Fix bug when terraform refreshing cloud run service

--- gcp/ftp-to-gcs/run.tf
+++ gcp/ftp-to-gcs/run.tf
@@ -1,36 +1,30 @@
 # Enables the Cloud Run API
 resource "google_project_service" "run_api" {
   service = "run.googleapis.com"
-
-  disable_on_destroy = true
 }
 
 # Create the Cloud Run service
 resource "google_cloud_run_service" "run_service" {
-  name     = "ftp-to-gcs"
-  location = var.region
+  name                       = "ftp-to-gcs"
+  location                   = var.region
+  autogenerate_revision_name = true
 
   template {
     spec {
       containers {
         image = "gcr.io/${var.project}/ftp-to-gcs:latest"
-        resources {
-          requests = {
-            memory = "512Mi"
-            cpu    = "500m"
-          }
-        }
+        resources {}
       }
       container_concurrency = 1
-      timeout_seconds       = 600
+      timeout_seconds       = 150
     }
 
     metadata {
       annotations = {
-        "autoscaling.knative.dev/maxScale"  = "5"
+        "autoscaling.knative.dev/maxScale"  = "3"
         "client.knative.dev/user-image"     = "gcr.io/${var.project}/ftp-to-gcs:latest"
         "run.googleapis.com/client-name"    = "gcloud"
-        "run.googleapis.com/client-version" = "399.0.0"
+        "run.googleapis.com/client-version" = "400.0.0"
       }
     }
   }
